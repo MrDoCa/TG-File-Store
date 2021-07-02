f2f6e9b44e1efa4c219c566ba68b5f331c07a3ef
MrDoCa <80938620+MrDoCa@users.noreply.github.com>
2021-07-02 18:13:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,7 +33,7 @@ async def start(c, m, cb=False):
 `You Can Add Your Movie Files To Our Group Using This Bot . You can also store your Telegram Media for permanent Link! `
 
 
-*🔅 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ:-** [ᴅᴏᴄ ᴅx](https://t.me/Dx_Doc)
+*🔅 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ:-** [ᴅᴏᴄ ᴅx](@Dx_Doc)
 """
 
     # Buttons
